ac8ea118b938d16d43bad7cebe24fbf3e221f8e4
Oscar Esteban <code@oscaresteban.es>
2023-10-13 18:02:53 +0200

    enh: port from tobii plugin

--- psychopy_eyetracker_sr_research/__init__.py
+++ psychopy_eyetracker_sr_research/__init__.py
@@ -5,8 +5,6 @@
 # Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2023 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
 
-"""Extension package for PsychoPy which adds support for various hardware
-devices by Tobii.
-"""
+"""Eyelink"""
 
 __version__ = "0.0.1"
